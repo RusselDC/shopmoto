8a81d46cd324a5e6d516bbc854bb0b4312bccfb5
RusselDC <delacruzruss12@gmail.com>
2024-10-06 08:55:56 +0800

    remove comments

--- app.py
+++ app.py
@@ -3,11 +3,3 @@ import bcrypt
 
 runner = AbstractShopDao()
 
-#password = 'Russeldc189'
-#hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-#print(bcrypt.checkpw("Russeldc189".encode('utf-8'), hashed))
-#print(runner.write("""INSERT INTO users
-#(user_id, user_name, user_email, user_contact_number, user_password)
-#VALUES (%s, %s, %s, %s, %s) returning *;
-#""",('ABC1234567','russel_dc','delacruzruss12@gmail.com',9215774058,bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()))))
-
